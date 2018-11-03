f9455bf24c8ab8b08a19f4aaabb3e0b7714c63ea
Bipin Banathur <bipinbanathur@gmail.com>
2018-11-03 22:56:36 +0530

    Java FX Examples

---
+++ javafx/src/com/rtaware/ui/MainController.java
@@ -0,0 +1,31 @@
+package com.rtaware.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class MainController implements Initializable 
+{
+    
+	@FXML
+    private void handleFileNew(ActionEvent event)
+	{
+		System.out.println("Hello World");
+    }
+	
+	@FXML
+    private void onFileClose(ActionEvent event)
+	{
+		System.out.println("File Close");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+	{
+	
+    }    
+	
+}
