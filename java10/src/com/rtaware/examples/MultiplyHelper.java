d203b375fa32b861a4de08474c4e46d9aa9cac28
Bipin Banathur <bipinbanathur@gmail.com>
2018-11-04 11:39:24 +0530

    Formatted Content

--- java10/src/com/rtaware/examples/MultiplyHelper.java
+++ java10/src/com/rtaware/examples/MultiplyHelper.java
@@ -8,20 +8,19 @@ public class MultiplyHelper
 	public static void main(String[] args)
 	{
 		@SuppressWarnings("deprecation")
-		Multiplier multiplier = ( oper1,  oper2) ->
+		Multiplier multiplier = (oper1, oper2) ->
 		{
 			Consumer<String> c = System.out::println;
 			c.accept(new Integer(oper1).toString());
 			c.accept(new Integer(oper2).toString());
-			return oper1*oper2;
+			return oper1 * oper2;
 		};
 		int result = multiplier.multiply(100, 101);
 		Consumer<Integer> c = System.out::println;
 		c.accept(result);
-		
-		int aa = 12,bb =65 ,cc =10;
-		int biggest = (aa > bb) ? ((aa > cc) ? aa : cc ): ( (bb > cc) ? bb: cc	);
+
+		int aa = 12, bb = 65, cc = 10;
+		int biggest = (aa > bb) ? ((aa > cc) ? aa : cc) : ((bb > cc) ? bb : cc);
 		System.out.println(biggest);
 	}
 }
-
